00a2675cd196d60e0ad6c1d21008c6e498ff9ed7
taoshiqian <17210240200@fudan.edu.cn>
2018-06-20 14:08:02 +0800

    利用递归去遍历多重dict

--- txt-wls/recursion_dict.py
+++ txt-wls/recursion_dict.py
@@ -1,15 +1,32 @@
 # -*- coding:utf-8 -*-
-
-
+# 递归实现多重for循环的函数
 def fn(_dict, depth):
     if depth == 1:
         for k, v in _dict.items():
             yield k, v
     else:
         for k, v in _dict.items():
-            yield from ( (k, *q) for q in fn(v, depth - 1) )
+            yield from ((k, *q) for q in fn(v, depth - 1))
+
+# 双重dict
+_dict = {
+    'A': {
+        'A1': [1, 2],
+        'A2': [3, 4]
+    },
+    'B': {
+        'B1': [21, 22],
+        'B2': [23, 24]
+    }
+}
+# 一句话遍历双重dict
+for k, v, x in fn(_dict, 2):
+    print(k, v, x)
+print()
+print()
 
 
+# 三重dict
 _dict = {
     # 'A': {
     #     'A1': [1, 2],
@@ -21,28 +38,28 @@ _dict = {
     # },
     'C': {
         'C1': {
-            'c11':'c11'
+            'c11': 'c11'
         },
         'C2': {
-            'c21':'c21',
-            'c22':'c22'
+            'c21': 'c21',
+            'c22': 'c22'
+        }
+    },
+    'D': {
+        'D1': {
+            'd11': 123
         }
     }
 }
-
+# 一句话遍历三重dict
 for k, v, x, y in fn(_dict, 3):
     print(k, v, x, y)
+print()
+print()
 
 
-_dict = {
-    'A': {
-        'A1': [1, 2],
-        'A2': [3, 4]
-    },
-    'B': {
-        'B1': [21, 22],
-        'B2': [23, 24]
-    }
-}
-for k, v, x in fn(_dict, 2):
-    print(k, v, x)
+# 土方法的三重循环,太丑了
+for k1, v1 in _dict.items():
+    for k2, v2 in v1.items():
+        for k3, v3 in v2.items():
+            print(k1, k2, k3, v3)
